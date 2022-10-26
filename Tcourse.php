1623cd51281dd7495b6cd6b879c4b397544e4280
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-10-25 19:26:44 -0500

    Update Tcourse.php

--- Tcourse.php
+++ Tcourse.php
@@ -25,31 +25,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  switch ($_POST['saveType']) {
-    case 'Add':
-      $sqlAdd = "insert into Instructor (instructor_name) value (?)";
-      $stmtAdd = $conn->prepare($sqlAdd);
-      $stmtAdd->bind_param("s", $_POST['iName']);
-      $stmtAdd->execute();
-      echo '<div class="alert alert-success" role="alert">New instructor added.</div>';
-      break;
-    case 'Edit':
-      $sqlEdit = "update instructor set Instructor_name=? where instructor_id=?";
-      $stmtEdit = $conn->prepare($sqlEdit);
-      $stmtEdit->bind_param("si", $_POST['iName'], $_POST['iid']);
-      $stmtEdit->execute();
-      echo '<div class="alert alert-success" role="alert">Instructor edited.</div>';
-      break;
-    case 'Delete':
-      $sqlDelete = "delete from Instructor where instructor_id=?";
-      $stmtDelete = $conn->prepare($sqlDelete);
-      $stmtDelete->bind_param("i", $_POST['iid']);
-      $stmtDelete->execute();
-      echo '<div class="alert alert-success" role="alert">Instructor deleted.</div>';
-      break;
-  }
-}
 
 $sql = "SELECT * from course";
 $result = $conn->query($sql);
@@ -60,13 +35,55 @@ if ($result->num_rows > 0) {
 ?>
   <tr>
     <td><?=$row["course_id"]?></td>
-           
-    
-    
     <td><?=$row["prefix"]?></td>
     <td><?=$row["number"]?></td>
     <td><?=$row["description"]?></td>
     <td>
+       <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editCustomer1">
+                Edit
+              </button>
+              <div class="modal fade" id="editCustomer1" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editCustomer1Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editCustomer1Label">Edit Customer</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editCustomer1Name" class="form-label">Customer Name</label>
+                          <input type="text" class="form-control" id="editCustomer1Name" aria-describedby="editCustomer1Help" name="cName" value="Tony Romo">
+                          <div id="editCustomer1Help" class="form-text">Enter the Customer's name.</div>
+                          <label for="EmployeeID" class="form-label">Employee ID</label>
+                          <input type="text" class="form-control" id="sid" aria-describedby="nameHelp" name="eid" value="1">
+                          <div id="nameHelp" class="form-text">Enter the Employee's ID</div>
+                          <label for="ProductName" class="form-label">Product Name</label>
+                          <input type="text" class="form-control" id="pName" aria-describedby="nameHelp" name="pName" value="Football">
+                          <div id="nameHelp" class="form-text">Enter the Product Name</div>
+                          <label for="ProductCost" class="form-label">Product Cost</label>
+                          <input type="text" class="form-control" id="pCost" aria-describedby="nameHelp" name="pCost" value="19.99">
+                          <div id="nameHelp" class="form-text">Enter the Product's cost</div>
+                        </div>
+                        <input type="hidden" name="cid" value="1">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <form method="post" action="">
+                <input type="hidden" name="cid" value="1" />
+                <input type="hidden" name="saveType" value="Delete">
+                <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
+              </form>
+            </td>
+          </tr>
+    
+    
       <form method="post" action="course-section.php">
         <input type="hidden" name="id" value="<?=$row["course_id"]?>" />
         <input type="submit" value="Sections" />
