10436e1cf21293eeef4482171b45589f78806c27
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-10-26 12:51:52 -0500

    Update Tstudent.php

--- Tstudent.php
+++ Tstudent.php
@@ -1,15 +1,8 @@
 <?php
   include "header.php";
 ?>
-        <h1>Students</h1>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>Student ID</th>
-      <th>Student Name</th>
 
-    </tr>
-    <?php
+<?php
 $servername = "localhost";
 $username = "buidemac_homework3";
 $password = "homework3user";
@@ -21,21 +14,93 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "select student_id, student_name from student";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+    case 'Add':
+      $sqlAdd = "insert into student (student_name) value (?)";
+      $stmtAdd = $conn->prepare($sqlAdd);
+      $stmtAdd->bind_param("st", $_POST['stName']);
+      $stmtAdd->execute();
+      echo '<div class="alert alert-success" role="alert">New student added.</div>';
+      break;
+    case 'Edit':
+      $sqlEdit = "update instructor set student_name=? where student_id=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("stp", $_POST['stName'], $_POST['stid']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">student edited.</div>';
+      break;
+    case 'Delete':
+      $sqlDelete = "delete from student where student_id=?";
+      $stmtDelete = $conn->prepare($sqlDelete);
+      $stmtDelete->bind_param("p", $_POST['stid']);
+      $stmtDelete->execute();
+      echo '<div class="alert alert-success" role="alert">student deleted.</div>';
+      break;
+  }
+}
+?>
+    
+      <h1>student</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          
+<?php
+$sql = "SELECT student_id, student_name from student";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-
-  </thead>
-  <tbody>
-  <tr>
-    <td><?=$row["student_id"]?></td>
-    <td><?=$row["student_name"]?></td>
-  </tr>
+          
+          <tr>
+            <td><?=$row["student_id"]?></td>
+            <td><?=$row["student_name"]?></td>
+            <td>
+              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editStudent<?=$row["student_id"]?>">
+                Edit
+              </button>
+              <div class="modal fade" id="editStudent<?=$row["student_id"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editStudent<?=$row["student_id"]?>Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editStudent<?=$row["student_id"]?>Label">Edit Student</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editStudent<?=$row["student_id"]?>Name" class="form-label">Name</label>
+                          <input type="text" class="form-control" id="editStudent<?=$row["student_id"]?>Name" aria-describedby="editStudent<?=$row["student_id"]?>Help" name="stName" value="<?=$row['student_name']?>">
+                          <div id="editStudent<?=$row["student_id"]?>Help" class="form-text">Enter the student's name.</div>
+                        </div>
+                        <input type="hidden" name="iid" value="<?=$row['student_id']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <form method="post" action="">
+                <input type="hidden" name="iid" value="<?=$row["student_id"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
+              </form>
+            </td>
+          </tr>
+          
 <?php
   }
 } else {
@@ -43,8 +108,38 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-  </tbody>
-    </table>
+          
+        </tbody>
+      </table>
+      <br />
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addStudent">
+        Add New
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addStudent" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addStudentLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addStudentLabel">Add Student</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="studentName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="studentName" aria-describedby="stnameHelp" name="stName">
+                  <div id="nameHelp" class="form-text">Enter the student's name.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
