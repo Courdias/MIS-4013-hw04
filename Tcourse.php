c41a53e7ce3fbd24f775895e2fdfa1ec08d2bebf
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-10-26 14:23:11 -0500

    Update Tcourse.php

--- Tcourse.php
+++ Tcourse.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       echo '<div class="alert alert-success" role="alert">New course added.</div>';
       break;
     case 'Edit':
-      $sqlEdit = "update course set prefix=?, number=?, description=?, where course_id=?";
+      $sqlEdit = "update course set prefix=?, number=?, description=? where course_id=?";
       $stmtEdit = $conn->prepare($sqlEdit);
       $stmtEdit->bind_param("sisi", $_POST['cpf'], $_POST['cnb'], $_POST['cdscr'], $_POST['cid']);
       $stmtEdit->execute();
