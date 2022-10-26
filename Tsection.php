82dfeb7977fe67991895172bc27554979bb98dfd
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-10-25 20:10:00 -0500

    Update Tsection.php

--- Tsection.php
+++ Tsection.php
@@ -67,7 +67,7 @@ if ($result->num_rows > 0) {
     <td><?=$row["section_number"]?></td>
     <td><?=$row["instructor_name"]?></td>
     <td>
-      <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editInstructor<?=$row["instructor_id"]?>">
+      <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editsection<?=$row["section_number"]?>">
                 Edit
               </button>
               <div class="modal fade" id="editInstructor<?=$row["instructor_id"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editInstructor<?=$row["instructor_id"]?>Label" aria-hidden="true">
