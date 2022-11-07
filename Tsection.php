44989d84cfe203f362b146039930280ade3f1873
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 14:12:39 -0600

    Update Tsection.php

--- Tsection.php
+++ Tsection.php
@@ -65,7 +65,7 @@ if ($result->num_rows > 0) {
   </tr>
      <td>
               <form method="post" action="">
-                <input type="hidden" name="id" value="<?=$row["student_id"]?>" />
+                <input type="hidden" name="id" value="<?=$row["section_id"]?>" />
                 <input type="hidden" name="saveType" value="Delete">
                 <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
               </form>
@@ -78,8 +78,37 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-  </tbody>
-    </table>
+         </tbody>
+      </table>
+      <br />
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addStudent">
+        Add New
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addStudent" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addStudentLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addStudentLabel">Add Student</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="studentName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="studentName" aria-describedby="snameHelp" name="sName">
+                  <div id="nameHelp" class="form-text">Enter the student's name.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
