3de793a54b033c2bd224edb847f5f3841e547f6a
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-11-07 14:17:51 -0600

    Update Tsection.php

--- Tsection.php
+++ Tsection.php
@@ -28,18 +28,18 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
-      $sqlAdd = "insert into student (student_name) value (?)";
+      $sqlAdd = "insert into course (prefix, number, description) value (?,?,?)";
       $stmtAdd = $conn->prepare($sqlAdd);
-      $stmtAdd->bind_param("s", $_POST['sName']);
+      $stmtAdd->bind_param("sis", $_POST['cpf'], $_POST['cnb'], $_POST['cdscr']);
       $stmtAdd->execute();
-      echo '<div class="alert alert-success" role="alert">New student added.</div>';
+      echo '<div class="alert alert-success" role="alert">New course added.</div>';
       break;
     case 'Delete':
-      $sqlDelete = "delete from student where student_id=?";
+      $sqlDelete = "delete from course where course_id=?";
       $stmtDelete = $conn->prepare($sqlDelete);
-      $stmtDelete->bind_param("i", $_POST['iid']);
+      $stmtDelete->bind_param("i", $_POST['cid']);
       $stmtDelete->execute();
-      echo '<div class="alert alert-success" role="alert">Student deleted.</div>';
+      echo '<div class="alert alert-success" role="alert">Course deleted.</div>';
       break;
   }
 }
@@ -78,28 +78,41 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-         </tbody>
+        </tbody>
       </table>
       <br />
       <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addStudent">
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCourse">
         Add New
       </button>
 
       <!-- Modal -->
-      <div class="modal fade" id="addStudent" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addStudentLabel" aria-hidden="true">
+      <div class="modal fade" id="addCourse" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addCourseLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="addStudentLabel">Add Student</h1>
+              <h1 class="modal-title fs-5" id="addCourseLabel">Add Course</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
               <form method="post" action="">
                 <div class="mb-3">
-                  <label for="studentName" class="form-label">Name</label>
-                  <input type="text" class="form-control" id="studentName" aria-describedby="snameHelp" name="sName">
-                  <div id="nameHelp" class="form-text">Enter the student's name.</div>
+                  <label for="prefix" class="form-label">Course Prefix</label>
+                  <input type="text" class="form-control" id="cpf" aria-describedby="courseHelp" name="cpf">
+                  <div id="courseHelp" class="form-text">Enter the course prefix.</div>
+                  
+                   <label for="number" class="form-label">Course Number</label>
+                   <input type="text" class="form-control" id="cnb" aria-describedby="nameHelp" name="cnb">
+                   <div id="nameHelp" class="form-text">Enter the course number.</div>
+                  
+                   <label for="description" class="form-label">Course Section</label>
+                   <input type="text" class="form-control" id="csect" aria-describedby="courseHelp" name="csect">
+                   <div id="nameHelp" class="form-text">Enter the course description.</div>
+                  
+                   <label for="description" class="form-label">Instructor</label>
+                   <input type="text" class="form-control" id="cins" aria-describedby="courseHelp" name="cins">
+                   <div id="nameHelp" class="form-text">Enter the course instructor.</div>
+                  
                 </div>
                 <input type="hidden" name="saveType" value="Add">
                 <button type="submit" class="btn btn-primary">Submit</button>
@@ -109,6 +122,6 @@ $conn->close();
         </div>
       </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
