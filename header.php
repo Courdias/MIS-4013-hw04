b6b239cf56db18225aaeafaffa530a9ee2359a4d
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-12-16 21:31:42 -0600

    Update header.php

--- header.php
+++ header.php
@@ -30,6 +30,9 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="Tstudent.php">Students</a>
         </li>
+         <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="Tta.php">Teacher Assistants</a>
+        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Non-Relevant Dropdown
